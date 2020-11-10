da20b96506c34ca4bf0db9c81183276f1da66cdc
Elton Fonseca <elton869@gmail.com>
2020-11-10 16:54:08 -0300

    first commit

---
+++ app/Http/Controllers/ApresentacaoController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use PDF;
+
+class ApresentacaoController
+{
+    public function olaMundo()
+    {
+        $domPdf = PDF::loadHTML('<h1 style="color: red">Ola Treinaweb</h1>');
+
+        return $domPdf->stream();
+    }
+
+    public function cursos()
+    {
+        $cursos = [
+            'php' => [
+                'nome' => 'curso PHP',
+                'versao' => 8
+            ],
+            'java' => [
+                'nome' => 'curso de Java',
+                'versao' => 12
+            ]
+        ];
+
+        $domPdf = PDF::loadView('cursos', compact('cursos'));
+
+        return $domPdf->stream();
+    }
+
+    public function wiki()
+    {
+        $domPdf = PDF::loadFile(public_path() . '/treinaweb-wiki.html');
+
+        //return $domPdf->stream();
+        return $domPdf
+            ->setPaper('a3', 'landscape')
+            ->save(public_path() . '/treinaweb.pdf')
+            ->stream();
+    }
+}
